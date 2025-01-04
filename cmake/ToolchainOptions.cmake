98ea12d393c588ea79d76c2448ac2d5953391e3b
Alexander Hück <hueck.alexander@gmail.com>
2025-01-04 10:58:35 +0100

    Extend CI

--- cmake/ToolchainOptions.cmake
+++ cmake/ToolchainOptions.cmake
@@ -1,10 +1,23 @@
 set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/modules)
 
-# Setup of all necessary include/lib dirs for the development of a Clang based tool
-find_package(LLVM REQUIRED)
-find_package(Clang REQUIRED)
+find_package(LLVM CONFIG HINTS "${LLVM_DIR}")
+if(NOT LLVM_FOUND)
+  message(STATUS "LLVM not found at: ${LLVM_DIR}.")
+  find_package(LLVM REQUIRED CONFIG)
+endif()
+
+set_package_properties(LLVM PROPERTIES
+  URL https://llvm.org/
+  TYPE REQUIRED
+  PURPOSE
+  "LLVM framework installation required to compile."
+)
+
+message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 
 list(APPEND CMAKE_MODULE_PATH "${LLVM_CMAKE_DIR}")
+
+find_package(Clang REQUIRED)
 
 include(AddLLVM)
 include(clang-tidy)
@@ -12,7 +25,7 @@ include(clang-format)
 include(log-util)
 include(target-util)
 
-set(LOG_LEVEL 0 CACHE STRING "Granularity of the logger. 3 ist most verbose, 0 is least.")
+set(LOG_LEVEL 0 CACHE STRING "Granularity of the logger. 3 is most verbose, 0 is least.")
 
 if (NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE Debug CACHE STRING "" FORCE)
